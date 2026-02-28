23fadcda3cac8d068cbadc0f67232e2c98a45ccf
sharathcx <sharathcx@users.noreply.github.com>
2026-02-28 21:45:57 +0530

    schema added

--- src/modules/user/user.schemas.ts
+++ src/modules/user/user.schemas.ts
@@ -1,10 +1,10 @@
 import { email, z } from "zod";
 
 export const UserPayloadSchema = z.object({
-    body: {
+    body: z.object({
         email: z.string().nonempty(),
         active: z.boolean().default(true)
-    }
+    })
 })
 
 
